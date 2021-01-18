5763aace50b4bf435b630c42167430dce31bc651
hodoosol <hodoosol@naver.com>
2021-01-18 23:04:25 +0900

    first commit

---
+++ 210118.py
@@ -0,0 +1,72 @@
+# 1003
+print("Hello\nWorld")
+
+
+# 1004
+print("'Hello'")
+
+
+# 1005
+print("\"Hello World\"")
+
+
+# 1006
+print("\"!@#$%^&*()\"")
+
+
+# 1007
+print("\"C:\Download\hello.cpp\"")
+
+
+# 1008
+print("\u250C\u252C\u2510\n\u251C\u253C\u2524\n\u2514\u2534\u2518")
+
+
+# 1010
+n = input()
+print(n)
+
+
+# 1011
+x = input()
+print(x)
+
+
+# 1012
+x = float(input())
+print("%.6f" %x)
+
+
+# 1013
+a, b = map(int, input().split())
+print(a, b)
+
+
+# 1014
+x, y = input().split()
+print(y, x)
+
+
+# 1015
+a = float(input())
+print("{0:.2f}".format(round(a, 2)))
+
+
+# 1017
+a = int(input())
+print(a, a, a)
+
+
+# 1018
+a, b = input().split(':')
+print(a + ':' + b)
+
+
+# 1019
+a, b, c = map(int, input().split('.'))
+print("%04d" %a, "%02d" %b, "%02d" %c, sep='.')
+
+
+# 1020
+a, b = input().split('-')
+print(a + b)
