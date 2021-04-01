75bb0a161be62f5b28ecf9feb848974b8566035b
이다솔 <hodoosol@naver.com>
2021-04-01 23:57:39 +0900

    Add files via upload

---
+++ 210401.py
@@ -0,0 +1,71 @@
+# 6749
+third = int(input())
+second = int(input())
+
+print(second + (second - third))
+
+
+
+
+
+# 5339
+print('     /~\\')
+print('    ( oo|')
+print('    _\=/_')
+print('   /  _  \\')
+print('  //|/.\|\\\\')
+print(' ||  \ /  ||')
+print('============')
+print('|          |')
+print('|          |')
+print('|          |')
+
+
+
+
+
+# 5338
+print('       _.-;;-._')
+print('\'-..-\'|   ||   |')
+print('\'-..-\'|_.-;;-._|')
+print('\'-..-\'|   ||   |')
+print('\'-..-\'|_.-\'\'-._|')
+
+
+
+
+
+# 5337
+print('.  .   .')
+print('|  | _ | _. _ ._ _  _')
+print('|/\|(/.|(_.(_)[ | )(/.')
+
+
+
+
+
+# 4892
+i = 1
+while True :
+    n = int(input())
+    if n == 0 :
+        break
+    if n % 2 == 0 :
+        print('{0}. even'.format(i), end=' ')
+        n //= 2
+    else :
+        n = (n - 1) // 2
+        print('{0}. odd'.format(i), end=' ')
+    print(n)
+    i += 1
+
+
+
+
+
+# 1550
+print(int(input(), 16))
+
+
+
+
