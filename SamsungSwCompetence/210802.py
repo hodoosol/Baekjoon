2a547326e4dd0e2291f2a9f8c73d0d68251eeaa0
이다솔 <hodoosol@naver.com>
2021-08-03 01:28:16 +0900

    Update 210802.py

--- SamsungSwCompetence/210802.py
+++ SamsungSwCompetence/210802.py
@@ -1,13 +1,1 @@
 # 3190 뱀, 골드 5
-n = int(input())
-board = [[0] * n for i in range(n)]
-for i in range(int(input())) :
-    a, b = map(int, input().split())
-    board[a - 1][b - 1] = 1
-
-direct = []
-for j in range(int(input())) :
-    x, c = map(str, input().split())
-    direct.append((int(x), c))
-
-d = [(0, 1), (1, 0), (0, -1), (-1, 0)]    
